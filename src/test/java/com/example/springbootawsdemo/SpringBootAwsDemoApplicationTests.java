62bed5fb2a33f803adfff2f80b192794bcd2d128
Saurabh Saxena <saurabh.jan03@gmail.com>
2023-11-13 23:42:45 -0500

    Spring boot aws demo

---
+++ src/test/java/com/example/springbootawsdemo/SpringBootAwsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootawsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
